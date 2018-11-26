b91b344428e0946b21251407a0f2efd172ead9bf
benjam <ben@octobox.io>
2018-11-26 16:52:29 +0000

    Checkpoint

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -6,6 +6,23 @@ class ContributionsController < ApplicationController
     respond_with @pull_requests
   end
 
+  def new
+    @contribution = Contribution.new
+  end
+
+  def create
+    @contribution = Contribution.new(event_params)
+    @contribution.user_id = current_user.id
+    if @contribution.save
+      redirect_to @contribution
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
   def meta
     @pull_requests_decorator = PullRequestsDecorator.new(pull_requests)
     respond_with @pull_requests_decorator
@@ -15,5 +32,9 @@ class ContributionsController < ApplicationController
 
   def pull_requests
     Contribution.year(current_year).order('created_at desc').includes(:user)
+  end
+
+  def contribution_params
+    params.require(:contribution).permit(:title, :body, :repo_name, :issue_url)
   end
 end
